e0c5954bc28c7f5d023374e439ed7dedd7b51dea
Nikhil Shetty <nikhilshetty3128@gmail.com>
2024-11-20 22:51:29 +0530

    deployment

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		console.log(process.env.MONGO_DB_URI,process.env.PORT);
 		await mongoose.connect(process.env.MONGO_DB_URI);
 		console.log("Connected to MongoDB");
 	} catch (error) {
